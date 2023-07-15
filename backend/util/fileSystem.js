2124c415d3085256e82b694875238efb44384f24
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-15 18:44:37 -0500

    update file system

--- backend/util/fileSystem.js
+++ backend/util/fileSystem.js
@@ -9,7 +9,23 @@ class FileSystem {
     }
     set setFilename(filename) {this.#filename = filename;}
     get getFilename() {return this.#filename}
-    saveFile(filename,updateContent) {return fs.writeFileSync(filename,updateContent,'utf8')}
-    getFile(filename) {return fs.readFileSync(filename,'utf8')}
+    saveFile(filename,updateContent) {
+        try {
+            console.log(JSON.stringify(updateContent))
+            fs.writeFileSync(filename,JSON.stringify(updateContent),'utf8')
+            console.log("saved")
+        } catch {
+            console.log("err")
+        }
+    }
+    getFile(filename) {return (JSON.parse(fs.readFileSync(filename,'utf8')))}
 }
+const data = {
+        "data": [
+            {"title":"app","id":"2"}
+        ]
+}
+let fileSystem = new FileSystem()
+fileSystem.saveFile(path.join(__dirname,'../cart.json'),data)
+console.log(fileSystem.getFile('../cart.json'))
 module.exports = FileSystem
