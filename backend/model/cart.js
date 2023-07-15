f760e9282c15780f060ee7423b9b7443fc077430
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-14 20:34:52 -0500

    update cart model

--- backend/model/cart.js
+++ backend/model/cart.js
@@ -1,9 +1,24 @@
-class cart {
+const fs = require('fs');
+const path = require('path');
+const databaseFile = '../data.json'
+
+class Cart {
     constructor(id,createdTime,products,totalPrice) {
         this.id = id;
         this.createdTime = createdTime;
         this.products = products;
         this.totalPrice = totalPrice;
     }
-    
+    set setId(id) {this.id = id;}
+    set setCreatedTime(createdTime) {this.createdTime = createdTime}
+    set setProducts(products) {this.products = products}
+    set setTotalPrice(totalPrice) {this.totalPrice = totalPrice}
+    get getId() {return this.id}
+    get getCreatedTime() {return this.createdTime}
+    get getProducts() {return this.products}
+    get getTotalPrice() {return this.totalPrice}
+    getAllCart() {
+        console.log(path.join(__dirname, databaseFile))
+        return JSON.parse(fs.readFileSync(path.join(__dirname, databaseFile),'utf-8'))
+    }
 }
