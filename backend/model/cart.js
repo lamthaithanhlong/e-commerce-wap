61142e4bcdf567ae86959e0a2f941db86a5c7df1
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-14 20:02:07 -0500

    update cart structure

---
+++ backend/model/cart.js
@@ -0,0 +1,9 @@
+class cart {
+    constructor(id,createdTime,products,totalPrice) {
+        this.id = id;
+        this.createdTime = createdTime;
+        this.products = products;
+        this.totalPrice = totalPrice;
+    }
+    
+}
