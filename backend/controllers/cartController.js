922a6dba5d16f121494722c899c6f2a764733d17
bacha wondimu <bwondim3@montgomerycollege.edu>
2023-07-16 14:13:58 -0500

    update bacha branch

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,35 @@
+const Cart = require('../models/cart')
+const FileSystem = require('../util/fileSystem')
+const cart = new Cart();
+const fileSystem = new FileSystem('../cart.json')
+const data = fileSystem.getFile
+
+class CartController {
+    constructor() {}
+    getCarts() {
+        return data
+            .then((res) => {console.log(res)})
+            .catch((err) => {console.log(err)})
+    }
+    getAllProductsInCart() {
+        return data
+            .then((res) => {console.log(res.products)})
+    }
+    saveCart(id,createdTime,products,totalPrice) {
+        let item = {
+            id,
+            createdTime,
+            products: [
+                products
+            ],
+            totalPrice
+        }
+        return data
+            .then(() => fileSystem.saveFile = item)
+            .catch((err) => {console.log(err)})
+    }
+}
+let cartController = new CartController()
+cartController.saveCart()
+cartController.getAllProductsInCart()
+module.exports = CartController
