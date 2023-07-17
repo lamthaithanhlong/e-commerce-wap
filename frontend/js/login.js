261e711b1c5737b0b975a7dd5d3d5c61775fc0a3
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-16 22:52:06 -0500

    update code front end

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -4,46 +4,39 @@ class LoginPage {
   }
   init() {
     // this.submitLogin()
-    this.displayProducts()
     this.actionPerform()
   }
-  async submitLogin() {
-      // Get the username and password values from the form
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  // Create an object with the username and password
-  const data = {
-    username: "john",
-    password: "123"
-  };
-  await fetch('http://localhost:3000/customer/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then(res => console.log(res))
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-  }
-
-  displayProducts() {
-        const productList = document.getElementById('product-list');
-        fetch('http://localhost:3000/product', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(user => {
-                console.log(user)
-            })
-            .catch(error => {
-                console.log('Error fetching user information:', error);
-            });
-    }
+  submitLogin() {
+    const username = "john"; // Set the desired username
+    const password = "123"; // Set the desired password
+  
+    const data = {
+      "username": username,
+      "password": password
+    };
+  
+    console.log(data);
+    fetch('http://localhost:3000/customer/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json(); // Parse the response as JSON
+        } else {
+          throw new Error('Login request failed'); // Handle non-successful responses
+        }
+      })
+      .then(data => {
+        console.log(data); // Handle the login response data
+      })
+      .catch(error => {
+        console.log('Error occurred during login:', error);
+      });
+  }  
   actionPerform() {
     const self = this
     
