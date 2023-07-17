f46dd8f683b2687d460e3cc33e93976ff4b29955
bacha wondimu <bwondim3@montgomerycollege.edu>
2023-07-16 21:09:37 -0500

    c0f6e49 update display image

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,14 @@ app.use(cors())
 app.use(express.json())
 app.use('/product',ProductRouter)
 app.use('/order',OrderRouter)
-app.use('/customer',CustomerRouter)
+app.use('/customer', CustomerRouter)
+
+app.use((req, res, next) => {
+res.status(404).json({ error: req.url + ' API not supported!' });
+});
+app.use((err, req, res, next) => {
+res.status(500).json({ error: 'Something is wrong! Try later' }); 
+});
 
 app.listen("3000", () => {
     console.log("listening on 3000")
--- frontend/js/login.js
+++ frontend/js/login.js
@@ -5,7 +5,33 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  // Send a request to the server for authentication
-  // You can use the Fetch API or AJAX to send the request
-  // Handle the response accordingly
+  // Create an object with the username and password
+  const data = {
+    username: username,
+    password: password
+  };
+
+  // Send a POST request to the server for authentication
+  fetch('http://localhost:3000/customer/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    if (response.ok) {
+      // Successful authentication, handle the response here
+      console.log('Authentication successful!');
+      Location.href="../"
+
+    } else {
+      // Authentication failed, handle the response here
+      console.log('Authentication failed!');
+    }
+  })
+  .catch(error => {
+    // Handle any network or server errors here
+    console.log('An error occurred:', error);
+  });
 });
