8376b875eef2327d7b835eedd73fd936d2058362
Lam Thai Thanh Long <lamthaithanhlong@gmail.com>
2023-07-17 15:00:34 -0500

    update backend api issue handle

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -5,6 +5,7 @@ class SignUpPage {
 
   init() {
     this.attachEventListeners();
+    this.validateForm();
     this.generateUniqueID().then((nextId) => {
       console.log(nextId);
       document.getElementById('getId').textContent = "CurrentID: " + nextId;
@@ -24,11 +25,11 @@ class SignUpPage {
       })
       .catch(error => {
         console.log('Error occurred:', error);
+        throw error;
       });
   }
 
   submitSignUp() {
-    event.preventDefault();
     const username = document.getElementById('username').value;
     const name = document.getElementById('name').value;
     const phone = document.getElementById('phone').value;
@@ -54,12 +55,13 @@ class SignUpPage {
             window.location.assign('login.html');
             return response.json();
           } else {
-            document.getElementById("headerMessage").textContent = "Error Status " + response.status;
+            document.getElementById("headerMessage").textContent = "Whoos! it look like you have issue, someone you this username";
             return Promise.reject(new Error('Signup request failed'));
           }
         })
         .catch(error => {
           console.log('Error occurred during signup:', error);
+          throw error;
         });
     });
   }
@@ -72,6 +74,50 @@ class SignUpPage {
         self.submitSignUp();
       });
     }
+  }
+
+  validateForm() {
+    const self = this;
+    document.getElementById("submitSignUp").addEventListener("click", function (event) {
+      event.preventDefault();
+
+      var nameInput = document.getElementById("name");
+      var usernameInput = document.getElementById("username");
+      var phoneInput = document.getElementById("phone");
+      var passwordInput = document.getElementById("password");
+
+      var isValid = true;
+      var nameRegex = /^[a-zA-Z\s]+$/; // Only allow letters and spaces in the name
+      var usernameRegex = /^\w+$/; // Only allow alphanumeric characters and underscores in the username
+      var phoneRegex = /^\d{10}$/; // Require exactly 10 digits for the phone number
+      var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/; // Require at least 8 characters with at least one lowercase letter, one uppercase letter, and one digit in the password
+
+      if (!nameRegex.test(nameInput.value.trim())) {
+        isValid = false;
+        document.getElementById("headerMessage").textContent = "Please enter a valid name. Only allow letters and spaces in the name";
+      }
+
+      if (!usernameRegex.test(usernameInput.value.trim())) {
+        isValid = false;
+        document.getElementById("getId").textContent = "Please enter a valid username. Only allow alphanumeric characters and underscores in the username";
+      }
+
+      if (!phoneRegex.test(phoneInput.value.trim())) {
+        isValid = false;
+        document.getElementById("headerMessage").textContent = "Please enter a valid phone number. Require exactly 10 digits for the phone number";
+      }
+
+      if (!passwordRegex.test(passwordInput.value)) {
+        isValid = false;
+        document.getElementById("getId").textContent =
+          "Please enter a valid password. It must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, and one digit.";
+      }
+
+      // Submit the form if validation passes
+      if (isValid) {
+        self.submitSignUp();
+      }
+    });
   }
 }
 
